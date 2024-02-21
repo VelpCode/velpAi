70ac76ddbbf7b4f635c9f431146ae6b7258b5a9c
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-02-21 17:38:26 -0500

    geminicreate

---
+++ gemini-ai-app/src/components/Aiwithtext.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { GoogleGenerativeAI } from "@google/generative-ai"
+
+
+const Aiwithtext = () => {
+
+    const genAI = new GoogleGenerativeAI()
+
+
+}
