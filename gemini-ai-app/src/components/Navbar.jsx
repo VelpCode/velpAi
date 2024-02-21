f16ac6cd22e62e2b7f36283ece46d12d97246428
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-02-21 14:37:53 -0500

    tailwind setup

--- gemini-ai-app/src/components/Navbar.jsx
+++ gemini-ai-app/src/components/Navbar.jsx
@@ -3,13 +3,11 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-        <div className="navbar">
-            <ul>
-                <li>Home</li>
+        <div className="navbar flex mt-10">
+                <li className='t text-6xl'>Home</li>
                 <li>About</li>
                 <li>AI Art</li>
                 <li>Gemeni AI</li>
-            </ul>
         </div>
     </div>
 
