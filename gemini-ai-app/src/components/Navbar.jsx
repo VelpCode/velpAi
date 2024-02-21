e310a6740e5201f35f08b32b4accd25ecb1ffa02
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-02-21 14:26:38 -0500

    react ai app setup

---
+++ gemini-ai-app/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="navbar">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>AI Art</li>
+                <li>Gemeni AI</li>
+            </ul>
+        </div>
+    </div>
+
+    
+  )
+}
+
+export default Navbar
