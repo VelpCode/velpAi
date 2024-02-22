40372181933a253c8d57e6e6d3f111d8a5659512
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-02-21 19:34:36 -0500

    Retrograde

--- gemini-ai-app/src/components/ImageGenerator.jsx
+++ gemini-ai-app/src/components/ImageGenerator.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Velp from '../images/velp.jpg'
+import "./styles.css";
 
 
 const ImageGenerator = () => {
@@ -7,8 +8,9 @@ const ImageGenerator = () => {
     <div className = "ai-image-gen flex flex-col justify-center items-center mt-10">
         
         <div className="header text-5xl mb-10 ">Your Ai Image</div>
-        <img className = "inage h-[300px] w-[300px]" src = {Velp} />
-        <input type="text" className="searchname h-[20px] w-[50px]" />
+        <img className = "inage h-[300px] w-[300px] mb-5" src = {Velp} />
+        <input type="text" className="searchname h-[20px] w-[400px] p-8 rounded-lg" />
+        <button className='generate-button'>Generate</button>
     </div>
   )
 }
