85b92135e0c5ced966e4de178e328f5963a6be16
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-02-23 13:55:42 -0500

    tovenusnback

--- gemini-ai-app/src/components/Navbar.jsx
+++ gemini-ai-app/src/components/Navbar.jsx
@@ -3,18 +3,22 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-        <div className="navbar flex mt-3 align-middle items-center justify-between p-5">
-          <div className="logo text-3xl">TestGem</div>
-          <ul className='d flex gap-10'>
-                <li>Home</li>
-                <li>About</li>
-                <li>AI Art</li>
-                <li>Gemeni AI</li>
-          </ul>
-        </div>
-    </div>
+        <div className="navbar flex items-center justify-between p-8">
 
-    
+            <div className="logo">
+                <hi className="venus text-xl font-medium text-gray-700">
+                🌑 Venus
+                </hi>
+            </div>
+            <ul className='flex gap-10 font-light text-sm align-middle items-center justify-center'>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Community Art</li>
+                <li className='b rounded-xl bg-gray-950 text-gray-50 text-xs shadow-2xl p-[10px] font-semibold'>Sign Up</li>
+            </ul>
+        </div>
+
+    </div>
   )
 }
 
